e7cc10e4b1b49c421fa59d5a4190fddb58e76566
Adam <adam.fren@o2.pl>
2025-11-08 16:51:56 +0100

    graph walk with no data

--- start.py
+++ start.py
@@ -6,22 +6,28 @@ G = nx.Graph()
 #add edge method iterate with two for loops for all edges
 #G.add_node()
 #G.add_edge()
-nlist=[i for i in range(28)]
-G.add_nodes_from(nlist)
+num_nodes= range(28)
+for i in range(28):
+     G.add_node(i,UTM=[i**2,i**3])
+
+print(G.nodes.data())
 elist=[]
 choices = random.choices(range(100),k=280)
 for i in range(28):
     for j in range(0+10*i,10+10*i):
         if choices[j]%10-4<0:
-            t=random.choice(nlist)
+            t=random.choice(num_nodes)
             while t==i:
-                    t=random.choice(nlist)
-            G.add_edge(i,t,weight=(choices[j]%10)/10)
+                    t=random.choice(num_nodes)
+            G.add_edge(i,t,weight=1)
 
 elist=G.edges
-print(elist)
+weightlist=nx.get_edge_attributes(G,"weight")
 pos = nx.spring_layout(G, seed=7)  # positions for all nodes - seed for reproducibility
+#print(G.nodes.data)
 
+#path = nx.dijkstra_path(G,0,1)
+#print(path)
 # nodes
 nx.draw_networkx_nodes(G, pos, node_size=300)
 
